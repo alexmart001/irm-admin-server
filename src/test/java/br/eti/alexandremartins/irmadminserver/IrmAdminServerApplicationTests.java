de8087d72e665e02fda6fe4786f9f7f4c2e47a1b
Alexandre Martins <alexandre.martins.001@gmail.com>
2022-03-16 22:06:48 -0300

    first commit

---
+++ src/test/java/br/eti/alexandremartins/irmadminserver/IrmAdminServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.eti.alexandremartins.irmadminserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrmAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
